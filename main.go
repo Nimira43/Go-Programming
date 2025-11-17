4eeb7d7348760fa5d626d8348f7d6d285200a83c
Nimira43 <n.rabone72@aol.com>
2025-11-17 15:10:12 +0000

    shuffle = 2

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+import "math/rand"
+
 func main() {
 	cards := newDeckFromFile("list_of_cards")
-	cards.print()	
+	cards.print()
 }
 
 func (d deck) shuffle() {
-
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+	}
 }
