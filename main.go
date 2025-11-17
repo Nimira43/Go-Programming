75ad0cf9f7ac59a864686a0424feb979b5657e0d
Nimira43 <n.rabone72@aol.com>
2025-11-17 15:18:09 +0000

    Calling shuffle() in main()

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "math/rand"
-
 func main() {
-	cards := newDeckFromFile("list_of_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
-}
-
-func (d deck) shuffle() {
-	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
-
-		d[i], d[newPosition] = d[newPosition], d[i]
-	}
 }
